bc0ab1437d88af89b104e68b2316310bdf0fc16e
Daria Miller <mille534@wwu.edu>
2020-10-27 14:47:08 -0700

    Start of role

--- role.java
+++ role.java
@@ -1,2 +1,43 @@
 //Daria
 //Have it inside of a clas? Or sepearte -> Look at Upgrade.java and Level class for example
+//compiles
+
+import java.util.*;
+import java.io.*;
+
+public class role{
+
+    String name;
+    int level;
+    String line;
+    boolean onCard;
+
+    public role(){
+	return;
+    }
+
+    public String getName(){
+	return "";
+    }
+
+    public int getLevel(){
+        return 0;
+    }
+
+    public String getRole(){
+        return "";
+    }
+
+    public boolean isOnCard(){
+        return true;
+    }
+
+    public void setOnCard(){
+        return;
+    }
+
+    public static void main(String[] args){ //dont use
+
+    }
+
+}

bd9a2f14b2cfcbd1ce9943b9a938d849319fd20f
Daria Miller <mille534@wwu.edu>
2020-10-27 14:43:49 -0700

    Start of board

--- board.java
+++ board.java
@@ -1,1 +1,52 @@
 //Daria
+//complies
+import java.io.*;
+import java.util.*;
+
+public class board{
+
+    private ArrayList<String> roomList;
+    private int day;
+    private int numPlayers;
+    private int numRooms = 12;
+   // private static room[] rooms = new room[numRooms];
+
+    //private player play; //I think this needs to be capital
+
+    public board(){
+	return;
+    }
+
+    public void createBoard(){
+	return;
+    }
+
+    public int getNumPlayers(){  //may also need current player
+	return 0;
+    }
+
+    public void setAdjacentRooms(){  //may not need here
+	return;
+    }
+
+    public int getCurrNumRooms(){
+	return 0;
+    }
+
+    public void updateNumRooms(){
+	return;
+    }
+
+    public boolean isEndDay(){
+	return true;
+    }
+
+    public void resetBoard(){
+	return;
+    }
+
+    public static void main(String[] args){ //need to replace with static
+
+    }
+
+}

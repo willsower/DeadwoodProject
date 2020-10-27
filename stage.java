930baa0e7eb56ee81ed5eacae4a19758d2a2ddae
Daria Miller <mille534@wwu.edu>
2020-10-27 14:48:05 -0700

    Start of stage

--- stage.java
+++ stage.java
@@ -1,1 +1,42 @@
 //Daria
+//complies
+import java.io.*;
+import java.util.*;
+
+public class stage{
+
+    String setName;
+    int totSetCount;
+    int currSetCount;
+
+    public stage(){
+        return;
+    }
+
+    //getters
+    public int getTotSetCount(){
+	return 0;
+    }
+
+    public int getCurrSetCount(){
+	return 0;
+    }
+
+    //setter
+    public void setTotSetCount(){
+	return;
+    }
+
+    public void setCurrSetCount(){
+	return;
+    }
+
+    public void rmFinalScene(){ //might be in board or the equivalent of my class scene
+	return;
+    }
+
+    public static void main(String[] args){ //no
+
+    }
+
+}

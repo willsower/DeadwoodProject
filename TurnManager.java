bdb702e22274ab66f9b422871f2a3790212261d3
willsower <>
2020-10-29 16:05:49 -0700

    init Players
    Initialized players in turn manager class

--- TurnManager.java
+++ TurnManager.java
@@ -6,8 +6,31 @@ public class TurnManager {
     private int currentPlayer;
     private int nextPlayer;
 
+    //Turn manager initializes all players
     public TurnManager(int numPlayer) {
+        //Init num players array
+        players = new Player[numPlayer];
 
+        //Populate players
+        for (int i = 0; i < numPlayer; i++) {
+            switch (numPlayer) {
+                case 5:
+                    players[i] = new Player[i + 1, 1, 0, 2, "Trailers"];
+                    break;
+                case 6:
+                    players[i] = new Player[i + 1, 1, 0, 4, "Trailers"];
+                    break;
+                case 7:
+                    players[i] = new Player[i + 1, 2, 0, 0, "Trailers"];
+                    break;
+                case 8:
+                    players[i] = new Player[i + 1, 2, 0, 0, "Trailers"];
+                    break;
+                default:
+                    players[i] = new Player[i + 1, 0, 0, 0, "Trailers"];
+                    break;
+            }
+        }
     }
 
     //Get functions

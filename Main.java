6e99d78f4ce6219eb3e6c454ed22bae2136a58c3
Taichen <jatr812@gmail.com>
2020-11-02 17:35:48 -0800

    made singleton
    Implemented singleton class

--- Main.java
+++ Main.java
@@ -1,34 +1,13 @@
-import java.util.Scanner;
-
 //together!
 public class Main {
     public static void main(String[] args) {
         try {
-            Scanner myOb = new Scanner(System.in);
-
-            int numPlayers = getNumPlayers(myOb);
+            int numPlayers = UserInterface.getInstance().getNumPlayers();
             systemManager(numPlayers);
 
         } catch (Exception e) {
             System.out.println("Error = " + e);
         }
-    }
-
-    //Prompts user how many players will be playing
-    //Will throw player in loop until they enter 2-8 players
-    public static int getNumPlayers(Scanner myOb) {
-        String val;
-        int numberPlayers = 0;
-
-        while (!(numberPlayers >= 2 && numberPlayers <= 8)) {
-            System.out.println("How many players? (2 - 8) ");
-            val = myOb.nextLine();
-            try {
-                numberPlayers = Integer.parseInt(val);
-            } catch (NumberFormatException e) {}
-        }
-
-        return numberPlayers;
     }
 
     //Create system for players then run functionality
--- UserInterface.java
+++ UserInterface.java
@@ -1,3 +1,33 @@
+//Singleton class, will be where we get user input/output
+
+import java.util.Scanner;
+
 public class UserInterface {
-    
+    private static UserInterface instance = null;
+
+    //Create instance
+    public static UserInterface getInstance() {
+        if (instance == null) {
+            instance = new UserInterface(); 
+        }
+        return instance; 
+    }
+
+    //Prompts user how many players will be playing
+    //Will throw player in loop until they enter 2-8 players
+    public static int getNumPlayers() {
+        Scanner myOb = new Scanner(System.in);
+        String val;
+        int numberPlayers = 0;
+
+        while (!(numberPlayers >= 2 && numberPlayers <= 8)) {
+            System.out.println("How many players? (2 - 8) ");
+            val = myOb.nextLine();
+            try {
+                numberPlayers = Integer.parseInt(val);
+            } catch (NumberFormatException e) {}
+        }
+
+        return numberPlayers;
+    }
 }

e4f79270f0e33670eefe779ac9c1031e16dd9dd4
miller534 <mille534@wwu.edu>
2020-11-08 12:04:02 -0800

    implimenting Set.java

--- Board.java
+++ Board.java
@@ -1,66 +1,23 @@
-//Daria
-//complies!!!!
-import java.io.*;
-import java.util.*;
+
 
 public class Board{
 
-    private ArrayList<String> roomList;
-    private int day;
-    private int numPlayers;
-    private int numRooms = 12;
-    private boolean endOfDay = false;
-    private int currNumRooms = numRooms-2;
-   // private static room[] rooms = new room[numRooms];
+    private Board instance = null;
 
-    private Player players; //I think this needs to be capital
 
-   public Board(int numRooms, int numPlayers, int day ){
-        // this.numRooms = numRooms;
-        // this.numPlayers = numPlayers;
-	    // return;
-    }
 
-    //public void createBoard(){ // same as above function
-	//    return;
-    //}
 
-    public int getNumPlayers(){  //may also need current player
-	    return this.numPlayers;
-    }
-
-    public void setAdjacentRooms(){  //may not need here
-    
-    }
-
-    public int getCurrNumRooms(){
-        return currNumRooms;
-    }
-
-    public void updateNumRooms(int numRooms){
-	    currNumRooms--;
-        if (currNumRooms == 1){
-            endOfDay = true;
+    //create instance
+    public Board getInstance(){
+        if (instance == null){
+            instance = new Board();
         }
-        //comment
+        return instance;
     }
     
-    public boolean isEndDay(){
-	    return endOfDay;
-    }
     
-    public void resetBoard(){
-        //this.endOfDay = false;
-        currNumRooms = numRooms-2;
-        endOfDay = false;
-        //for(int i=0; i<numRooms-2; i++){
-            //
-        //}
-	    //return;
-    }
 
-    public static void main(String[] args){ //need to replace with static
 
-    }
+
 
 }
---
+++ Set.java
@@ -0,0 +1,105 @@
+
+
+public class Set{
+
+    
+    private String setName;
+    private int numberOfNeighbors;
+    private String[] neighbor= new String[numberOfNeighbors];
+    private int[] setArea = new int[4];
+    private int numberOfTakes;
+    private Take[] take = new Take[numberOfTakes];
+    private int numberOfParts;
+    private Part[] part = new Part[numberOfParts];
+
+    class Take {
+        int takeNumber;
+        int[] takeArea;
+    }
+
+    class Part {
+        String partName;
+        int level;
+        int[] partArea;
+        String line;
+    }
+
+    //constructor
+    public Set(String setName){
+        this.setName = setName;
+    }
+
+    //getters
+    public String getSetName(){
+        return setName;
+    }
+
+    public int getNumberOfNeighbors(){
+        return numberOfNeighbors;
+    }
+
+    public String[] getNeighbor(){
+        return neighbor;
+    }
+
+    public int[] getSetArea(){
+        return setArea;
+    }
+
+    public int getNumberOfTakes(){
+        return numberOfTakes;
+    }
+
+    public Take[] getTake(){
+        return take;
+    }
+
+    public int getNumberOfParts(){
+        return numberOfParts;
+    }
+
+    public Part[] getParts(){
+        return part;
+    }
+
+
+
+    //setters
+    public void setNumberOfNeighbors(int num){
+       numberOfNeighbors = num;
+    }
+
+    public void setNeighbors(String neighbor1, String neighbor2, String neighbor3 ){
+        neighbor[0] = neighbor1;
+        neighbor[1] = neighbor2;
+        neighbor[2] = neighbor3;
+    }
+
+    public void setNeighbors(String neighbor1, String neighbor2, String neighbor3, String neighbor4){
+        neighbor[0] = neighbor1;
+        neighbor[1] = neighbor2;
+        neighbor[2] = neighbor3;
+        neighbor[3] = neighbor4;
+    }
+
+    public void setSetArea(int x, int y, int h, int w){
+        setArea[0] = x;
+        setArea[1] = y;
+        setArea[2] = h;
+        setArea[3] = w;
+    }
+
+    public void setNumberOfTakes(int num){
+        numberOfTakes = num;
+    }
+
+    public void setTake(int num, int x, int y, int h, int w){
+        take[num-1].takeNumber = num;
+        take[num-1].takeArea[0] = x;
+        take[num-1].takeArea[1] = y;
+        take[num-1].takeArea[2] = h;
+        take[num-1].takeArea[3] = w;
+    }
+
+
+}

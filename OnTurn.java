6bca2d84b8c2e840c9c342a442e3aaffbde82539
Taichen <jatr812@gmail.com>
2020-11-08 18:51:07 -0800

    Updated format in onturn class
    Updated format

--- OnTurn.java
+++ OnTurn.java
@@ -1,20 +1,21 @@
+
 //Tai
-import java.util.Random; 
+import java.util.Random;
 
 public class OnTurn {
     public static void onMove(Player player) {
-        //give player option to move
-            //call user interface
-            //Interact with board?
+        // give player option to move
+        // call user interface
+        // Interact with board?
 
-        //if player moved to casting office
-            //allow upgrade possibility
+        // if player moved to casting office
+        // allow upgrade possibility
 
-        //if player moved to card area and card still has roles
-            //give option to choose roll
+        // if player moved to card area and card still has roles
+        // give option to choose roll
     }
 
-    public static void takeOnCardRole(Player player)  {
+    public static void takeOnCardRole(Player player) {
         player.setOnCardRole(true);
     }
 
@@ -22,25 +23,25 @@ public class OnTurn {
         player.setOffCardRole(true);
     }
 
-    //Function to see if player is allowed to rehearse.
-    //Player is not able to reherase if they already have 5 practice chips
+    // Function to see if player is allowed to rehearse.
+    // Player is not able to reherase if they already have 5 practice chips
     public static boolean canRehearse(int practiceChip) {
         if (practiceChip == 5) {
             return false;
         }
         return true;
-        //setter function in player will increase practice chip
+        // setter function in player will increase practice chip
     }
 
     public static boolean act() {
         return true;
     }
 
-    //Random number generater between 1 and 6, like a die roll
+    // Random number generater between 1 and 6, like a die roll
     public static int roll() {
         Random ran = new Random();
 
-        //Die rolls between 1 and 6
+        // Die rolls between 1 and 6
         int lowerBound = 1;
         int upperBound = 6;
 
@@ -49,9 +50,9 @@ public class OnTurn {
 
     public static void turn(Player player) {
 
-        //If player has not taken a role
+        // If player has not taken a role
         if (player.getOffCardRole() == false || player.getOnCardRole() == false) {
-            
+
         }
     }
 }

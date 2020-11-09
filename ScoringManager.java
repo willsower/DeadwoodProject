0a8910099137dcbf9cf3ba427cb8aad1b0b7943b
Taichen <jatr812@gmail.com>
2020-11-08 18:50:16 -0800

    Updated format in Scoring manager class
    Updated format

--- ScoringManager.java
+++ ScoringManager.java
@@ -1,31 +1,32 @@
+
 //Singleton
 //Tai
-import java.util.Random; 
+import java.util.Random;
 
 public class ScoringManager {
 
     private static ScoringManager instance = null;
 
-    //Create instance
+    // Create instance
     public static ScoringManager getInstance() {
         if (instance == null) {
-            instance = new ScoringManager(); 
+            instance = new ScoringManager();
         }
-        return instance; 
+        return instance;
     }
 
-    //Adds together user dollar and credits
+    // Adds together user dollar and credits
     public static int addDollarCredits(int dollar, int credit) {
         return dollar + credit;
     }
 
-    //Calculates points by player rank and adds dollar and credit total
+    // Calculates points by player rank and adds dollar and credit total
     public static int getRankPoints(int rank, int dollarCredit) {
         int tot = rank * 5;
         return tot + dollarCredit;
     }
 
-    //Gets player final score
+    // Gets player final score
     public static int finalScore(int rank, int dollar, int credit) {
         int firstTotal = addDollarCredits(dollar, credit);
         return getRankPoints(rank, firstTotal);
@@ -35,18 +36,18 @@ public class ScoringManager {
         return true;
     }
 
-    //Calculates payout value and will return the int values in an
-    //array
+    // Calculates payout value and will return the int values in an
+    // array
     public static int[] calculatePayout(int budget) {
         OnTurn turn = new OnTurn();
-        
+
         int highestRank = 0;
         int middleRank = 0;
         int lowestRank = 0;
         int[] total = new int[3];
 
-        //Rolls the amount of die equal to the budget.
-        //Will add to each level how much should earn
+        // Rolls the amount of die equal to the budget.
+        // Will add to each level how much should earn
         for (int i = 1; i <= budget; i++) {
             int temp = turn.roll();
             switch (i) {
@@ -56,13 +57,13 @@ public class ScoringManager {
                 case 2:
                     middleRank += temp;
                     break;
-                case 3: 
+                case 3:
                     lowestRank += temp;
                     break;
-                case 4: 
+                case 4:
                     highestRank += temp;
                     break;
-                case 5: 
+                case 5:
                     middleRank += temp;
                     break;
                 default:
@@ -71,7 +72,7 @@ public class ScoringManager {
             }
         }
 
-        //Populates array with payout values
+        // Populates array with payout values
         total[0] = highestRank;
         total[1] = middleRank;
         total[2] = lowestRank;
@@ -79,7 +80,7 @@ public class ScoringManager {
         return total;
     }
 
-    //Returns the payout depending on their role on the card
+    // Returns the payout depending on their role on the card
     public static int bonusOnCard(int priorityOnRole, int budget) {
         int[] total = calculatePayout(budget);
 
@@ -92,7 +93,7 @@ public class ScoringManager {
         }
     }
 
-    //Gives off card player money to the rank of their role 
+    // Gives off card player money to the rank of their role
     public static int bonusOffCard(int roleRank) {
         return roleRank;
     }

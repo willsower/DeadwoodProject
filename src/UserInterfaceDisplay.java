30fbfa123a37415a8130c8b54c38f1fd7165a4f3
willsower <>
2020-11-19 13:55:11 -0800

    Put all calls in system manager

--- src/UserInterfaceDisplay.java
+++ src/UserInterfaceDisplay.java
@@ -17,7 +17,7 @@ import javafx.scene.Scene;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class UserInterfaceDisplay implements Initializable {
+public class UserInterfaceDisplay /*implements Initializable*/ {
 
     private static UserInterfaceDisplay instance = null;
     // Create instance
@@ -28,110 +28,110 @@ public class UserInterfaceDisplay implements Initializable {
         return instance;
     }
 
-    @FXML private ImageView boardImage;
-    @FXML private ImageView deck;
-
-    // Card Image Views
-    @FXML private ImageView trainStationCard;
-    @FXML private ImageView jailCard;
-    @FXML private ImageView mainStreetCard;
-    @FXML private ImageView generalStoreCard;
-    @FXML private ImageView saloonCard;
-    @FXML private ImageView ranchCard;
-    @FXML private ImageView bankCard;
-    @FXML private ImageView secretHideoutCard;
-    @FXML private ImageView churchCard;
-    @FXML private ImageView hotelCard;
-
-    // Player's display information
-    @FXML private Label currentPlayer;
-    @FXML private Label playerDieColor;
-    @FXML private Label playerDollar;
-    @FXML private Label playerCredit;
-    @FXML private Label playerPracticeChip;
-
-    // Text Display
-    @FXML private Label dayDisplay; // Display current day
-    @FXML private Label displayText;
-    @FXML private TextField userInput;
-    @FXML private TextField displayNum; //to test take out later
-    @FXML private Button submitButton;
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        boardImage.setImage(Board.getInstance().getBoardImage());
-        boardImage.setVisible(false);
-        deck.setVisible(false);
-    }
-
-    //want this function to set the number of players and continue to the game
-    //right now all it does is print the string the user inputs then display board image
-    public void submitPlayers(ActionEvent event) {
-        String val = userInput.getText();
-        //setPlayerNum(playerNum);
-
-        //String val;
-        int numberPlayers = 0;
-
-        while (!(numberPlayers >= 2 && numberPlayers <= 8)) {
-            //System.out.println("How many players? (2 - 8) ");
-            //val = myOb.nextLine();
-            val = userInput.getText();
-            try {
-                numberPlayers = Integer.parseInt(val);
-            } catch (NumberFormatException e) {
-            }
-        }
-
-
-        displayNum.setText("number of players is " + val);
-
-        submitButton.setVisible(false); //may also nee to disable all of these
-        displayText.setVisible(false);
-        userInput.setVisible(false);
-        displayNum.setVisible(false);
-        //userInput.setDisable(true);
-
-//        setUpBoard(1, Integer.parseInt(playerNum));
-
-        SystemManager.getInstance(Integer.parseInt(val)).run();
-    }
-
-    // Sets board up at each day
-    public void setUpBoard(int day, int numPlayer) {
-        boardImage.setVisible(true);
-        deck.setVisible(true);
-
-        trainStationCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        jailCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        mainStreetCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        generalStoreCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        saloonCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        ranchCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        bankCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        secretHideoutCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        churchCard.setImage(Deck.getInstance().getBackOfCardSmall());
-        hotelCard.setImage(Deck.getInstance().getBackOfCardSmall());
-
-        dayDisplay.setText("Day " + day);
-
-        //Add players into trailers
-        for (int i = 0; i < numPlayer; i++) {
-
-        }
-
-        //close window --take out when continuing to run game
-//        Stage stage = (Stage) submitButton.getScene().getWindow();
-//        stage.close();
-    }
-
-    public void showCurrentPlayerInfo(int player, String color, int dollar, int credit, int practice) {
-        currentPlayer.setText("Current Player: " + player);
-        playerDieColor.setText("Die Color: " + color);
-        playerDollar.setText("Dollar: " + dollar);
-        playerCredit.setText("Credit " + credit);
-        playerPracticeChip.setText("Practice Chip: " + practice);
-    }
+//    @FXML private ImageView boardImage;
+//    @FXML private ImageView deck;
+//
+//    // Card Image Views
+//    @FXML private ImageView trainStationCard;
+//    @FXML private ImageView jailCard;
+//    @FXML private ImageView mainStreetCard;
+//    @FXML private ImageView generalStoreCard;
+//    @FXML private ImageView saloonCard;
+//    @FXML private ImageView ranchCard;
+//    @FXML private ImageView bankCard;
+//    @FXML private ImageView secretHideoutCard;
+//    @FXML private ImageView churchCard;
+//    @FXML private ImageView hotelCard;
+//
+//    // Player's display information
+//    @FXML private Label currentPlayer;
+//    @FXML private Label playerDieColor;
+//    @FXML private Label playerDollar;
+//    @FXML private Label playerCredit;
+//    @FXML private Label playerPracticeChip;
+//
+//    // Text Display
+//    @FXML private Label dayDisplay; // Display current day
+//    @FXML private Label displayText;
+//    @FXML private TextField userInput;
+//    @FXML private TextField displayNum; //to test take out later
+//    @FXML private Button submitButton;
+//
+//    @Override
+//    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        boardImage.setImage(Board.getInstance().getBoardImage());
+//        boardImage.setVisible(false);
+//        deck.setVisible(false);
+//    }
+//
+//    //want this function to set the number of players and continue to the game
+//    //right now all it does is print the string the user inputs then display board image
+//    public void submitPlayers(ActionEvent event) {
+//        String val = userInput.getText();
+//        //setPlayerNum(playerNum);
+//
+//        //String val;
+//        int numberPlayers = 0;
+//
+//        while (!(numberPlayers >= 2 && numberPlayers <= 8)) {
+//            //System.out.println("How many players? (2 - 8) ");
+//            //val = myOb.nextLine();
+//            val = userInput.getText();
+//            try {
+//                numberPlayers = Integer.parseInt(val);
+//            } catch (NumberFormatException e) {
+//            }
+//        }
+//
+//
+//        displayNum.setText("number of players is " + val);
+//
+//        submitButton.setVisible(false); //may also nee to disable all of these
+//        displayText.setVisible(false);
+//        userInput.setVisible(false);
+//        displayNum.setVisible(false);
+//        //userInput.setDisable(true);
+//
+////        setUpBoard(1, Integer.parseInt(playerNum));
+//
+////        SystemManager.getInstance(Integer.parseInt(val)).run();
+//    }
+//
+//    // Sets board up at each day
+//    public void setUpBoard(int day, int numPlayer) {
+//        boardImage.setVisible(true);
+//        deck.setVisible(true);
+//
+//        trainStationCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        jailCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        mainStreetCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        generalStoreCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        saloonCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        ranchCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        bankCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        secretHideoutCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        churchCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//        hotelCard.setImage(Deck.getInstance().getBackOfCardSmall());
+//
+//        dayDisplay.setText("Day " + day);
+//
+//        //Add players into trailers
+//        for (int i = 0; i < numPlayer; i++) {
+//
+//        }
+//
+//        //close window --take out when continuing to run game
+////        Stage stage = (Stage) submitButton.getScene().getWindow();
+////        stage.close();
+//    }
+//
+//    public void showCurrentPlayerInfo(int player, String color, int dollar, int credit, int practice) {
+//        currentPlayer.setText("Current Player: " + player);
+//        playerDieColor.setText("Die Color: " + color);
+//        playerDollar.setText("Dollar: " + dollar);
+//        playerCredit.setText("Credit " + credit);
+//        playerPracticeChip.setText("Practice Chip: " + practice);
+//    }
 
     // Display winners of game
     public void displayWinner(Integer[] finals) {

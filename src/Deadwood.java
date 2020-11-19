2fbc1dd7e5799014656e81af12e7c7f1be6646e3
willsower <>
2020-11-18 19:00:31 -0800

    Deleting unnecessary lines of code in deadwood

--- src/Deadwood.java
+++ src/Deadwood.java
@@ -25,24 +25,6 @@ public class Deadwood extends Application {
 
     public static void main(String[] args) {
         launch(args);
-//        Scanner myOb = new Scanner(System.in);
-//        String val = args[0];
-//        int numberPlayers = 0;
-//	    try {
-//            numberPlayers = Integer.parseInt(val);
-//        } catch (NumberFormatException nfe) {
-//        }
-//
-//        while (!(numberPlayers >= 2 && numberPlayers <= 8)) {
-//            System.out.println("How many players? (2 - 8) ");
-//            val = myOb.nextLine();
-//            try {
-//                numberPlayers = Integer.parseInt(val);
-//            } catch (NumberFormatException e) {
-//            }
-//        }
-//        systemManager(numberPlayers);
-
     }
 
     // Create system for players then run functionality

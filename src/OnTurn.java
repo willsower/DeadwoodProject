f897cd8e4d084187be00ea0ca57175b3a304582c
willsower <>
2020-11-25 18:48:52 -0800

    Changed my previous thing

--- src/OnTurn.java
+++ src/OnTurn.java
@@ -350,7 +350,7 @@ public class OnTurn {
                     players[i] = new Player(i + 1, 2, 0, 0, "trailer", playerDie[i]);
                     break;
                 default:
-                    players[i] = new Player(i + 1, 5, 0, 0, "trailer", playerDie[i]);
+                    players[i] = new Player(i + 1, 1, 0, 0, "trailer", playerDie[i]);
                     break;
             }
         }
